7438a5a479db76638e21e8b181cd0cd56b9c6e54
mathen <math.z.mhj@gmail.com>
2026-07-06 14:05:32 -0300

    feat(style): add variables for use in the project

---
+++ app/Utils/links.tsx
@@ -0,0 +1,18 @@
+export const linkIconInsta = "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/ig-instagram-icon.png"
+export const linkIconWpp = "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/whatsapp-color-icon.png"
+export const linkIconEmail = "https://uxwing.com/wp-content/themes/uxwing/download/communication-chat-call/red-mail-icon.png"
+export const linkIconInstaBlack = "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/black-instagram-icon.png"
+export const linkIconTel = "https://uxwing.com/wp-content/themes/uxwing/download/communication-chat-call/calling-icon.png"
+export const linkIconEmailBlack = "https://uxwing.com/wp-content/themes/uxwing/download/communication-chat-call/envelope-line-icon.png"
+export const linkIconTelBlack = "https://uxwing.com/wp-content/themes/uxwing/download/communication-chat-call/ringing-phone-outline-icon.png"
+export const linkIconLinkedin = "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/linkedin-app-icon.png"
+
+export const linkConfirm = "https://uxwing.com/wp-content/themes/uxwing/download/checkmark-cross/green-check-icon.png"
+
+const numberWpp = "558199219592"
+const email = "thayshelena.personaltrainer@gmail.com"
+export const linkWpp = `https://wa.me/${numberWpp}?text=Ol%C3%A1%20Thays%2C%20me%20interessei%20pelo%20seu%20trabalho.%20Estou%20querendo%20ingressar%20no%20TeamTH.`
+export const linkWppSimple = `https://wa.me/${numberWpp}?text=Ol%C3%A1%20Thays%2C%20me%20interessei%20pelo%20seu%20trabalho.%20Podemos%20conversar?`
+export const linkInsta = "https://www.instagram.com/thayshelena_personaltrainer?igsh=MXFocnc5ajN4Nnhu"
+export const linkLinkedin = "https://www.linkedin.com/in/thays-helena-bba7a4143"
+export const linkEmail = `mailto:${email}?subject=Interesse%20no%20TeamTH&body=Ol%C3%A1%20Thays%2C%0A%0AMe%20interessei%20pelo%20seu%20trabalho!`
