69d5f4e7a4e4adae9f24b71fa6dde171c395eed0
mathen <math.z.mhj@gmail.com>
2026-07-07 20:39:51 -0300

    chore: add new device to access project

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ['192.168.0.105', '192.168.0.106', '192.168.1.75']
+  allowedDevOrigins: ['192.168.0.105', '192.168.0.106', '192.168.1.75', '192.168.1.130']
 };
 
 export default nextConfig;
